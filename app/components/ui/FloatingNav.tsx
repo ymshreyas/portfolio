77b85775dff6ece5c8b6ada80188d2dd74e1f2a7
Shreyas YM <ymshreyas@gmail.com>
2025-03-17 15:43:05 +0530

    final changes

--- app/components/ui/FloatingNav.tsx
+++ app/components/ui/FloatingNav.tsx
@@ -9,15 +9,17 @@ import {
 import Link from "next/link";
 import { cn } from "../../../lib/utils";
 
+interface NavItem {
+  name: string;
+  link: string;
+  icon?: JSX.Element;
+}
+
 export const FloatingNav = ({
   navItems,
   className,
 }: {
-  navItems: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
+  navItems: NavItem[];
   className?: string;
 }) => {
   const { scrollYProgress } = useScroll();
@@ -28,7 +30,7 @@ export const FloatingNav = ({
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
     if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
+      const direction = current! - scrollYProgress.getPrevious()!;
 
       if (scrollYProgress.get() < 0.05) {
         // also set true for the initial state
@@ -42,7 +44,6 @@ export const FloatingNav = ({
       }
     }
   });
-
   return (
     <AnimatePresence mode="wait">
       <motion.div
@@ -68,7 +69,7 @@ export const FloatingNav = ({
           border: "1px solid rgba(255, 255, 255, 0.125)",
         }}
       >
-        {navItems.map((navItem: any, idx: number) => (
+        {navItems.map((navItem: NavItem, idx: number) => (
           <Link
             key={`link=${idx}`}
             href={navItem.link}
